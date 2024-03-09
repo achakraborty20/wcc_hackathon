a5ca391b4d45d5a49bb9bbdfe5a54e9725401c24
dgarciaperales <dgarciaperales04@gmail.com>
2024-03-09 16:42:56 -0500

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,37 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdown = document.getElementById('dropdown');
-    dropdown.addEventListener('focus', function () {
-        this.size=4;
-    });
-    dropdown.addEventListener('blur',function () {
-        this.size=1;
+// fetch('/get_data', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         occupation: selectedOccupation,
+//         gender: selectedGender,
+//         race: selectedRace
+//     })
+// })
+// .then(response => response.json())
+// .then(data => {
+//     // Update your HTML to display the data returned from Flask
+//     document.getElementById('results').innerHTML = `
+//         <p>Salary: ${data.salary}</p>
+//         <p>Percentage: ${data.percentage}</p>
+//     `;
+// })
+// .catch(error => {
+//     console.error('Error:', error);
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('fetchDataButton').addEventListener('click', function() {
+        fetch('/get_data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                
+                occupation: selectedOccupation,
+                gender: selectedGender,
+                race: selectedRace
+                
+                // Add any data you want to send to the Python function here
+            })
+        })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('results').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
 });
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdown2 = document.getElementById('dropdown2');
-    dropdown2.addEventListener('focus', function () {
-        this.size=5;
-    });
-    dropdown2.addEventListener('blur',function () {
-        this.size=1;
-    });
-});
-
-
-function handleRaceSelection() {
-    var selectedRace = document.getElementById("dropdown").value;
-    
-    var jsonData = JSON.stringify({"race": selectedRace});
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "dataPreprocessing", true);
-    xhr.setRequestHeader("Content-Type","application/json");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status == 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send(jsonData);
-}
