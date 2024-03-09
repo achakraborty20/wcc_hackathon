ccafefcee47678f4b50c23dbfe8c29bb776945a2
dgarciaperales <dgarciaperales04@gmail.com>
2024-03-09 15:11:18 -0500

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     var dropdown = document.getElementById('dropdown');
     dropdown.addEventListener('focus', function () {
-        this.size=5;
+        this.size=4;
     });
     dropdown.addEventListener('blur',function () {
+        this.size=1;
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var dropdown2 = document.getElementById('dropdown2');
+    dropdown2.addEventListener('focus', function () {
+        this.size=5;
+    });
+    dropdown2.addEventListener('blur',function () {
         this.size=1;
     });
 });
